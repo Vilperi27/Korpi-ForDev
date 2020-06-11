2955601b2920ee941f26e2171994aef764be5ad3
Nuutti´ <nuutti.jokinen@windowslive.com>
2020-06-11 08:41:44 +0300

    Update CSVtoCSV.py
    Turhaa Työtä lisättynä

---
+++ CSVtoCSV.py
@@ -0,0 +1,7 @@
+import csv
+
+
+with open('MyData.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
